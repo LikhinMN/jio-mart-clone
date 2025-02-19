8f92edc9bf95b27fec31957ba855b5a32829f8dd
LikhinMN <mnlikhin@gmail.com>
2025-02-19 17:39:16 +0530

    making home page interactive

--- data/product.js
+++ data/product.js
@@ -1,11 +1,13 @@
 const product = [
   {
+    id:'14134132454jk',
     Image: "./image/product.jpg",
     name: "Active White Detergent powder",
     OriginalPrice: 50000,
     discountPercebtage: 24,
   },
   {
+    id:'1413413245444hj',
     Image: "./image/product3.jpg",
     name: "Fybrod 3 Pin Plug Travel Socket Adaptor",
     OriginalPrice: 59900,
--- script/jioMart.js
+++ script/jioMart.js
@@ -10,16 +10,25 @@ product.forEach((product) => {
           </div>
           <div class="product-details">
             <div class="product-name"><p>${product.name}</p></div>
-            <div class="product-price">&#8377; ${((product.OriginalPrice)/100-(product.OriginalPrice*product.discountPercebtage/100)/100).toFixed(2)}</div>
+            <div class="product-price">&#8377; ${(
+              product.OriginalPrice / 100 -
+              (product.OriginalPrice * product.discountPercebtage) / 100 / 100
+            ).toFixed(2)}</div>
             <div class="product-discount-container">
               <div class="product-discount">
-                <div class="product-discount-price">&#8377; ${(product.OriginalPrice/100).toFixed(2)}</div>
-                <div class="product-discount-percentage">${product.discountPercebtage}% OFF</div>
+                <div class="product-discount-price">&#8377; ${(
+                  product.OriginalPrice / 100
+                ).toFixed(2)}</div>
+                <div class="product-discount-percentage">${
+                  product.discountPercebtage
+                }% OFF</div>
               </div>
             </div>
             <div class="product-button-container">
               <div class="product-add-button">
-                <button>Add</button>
+                <button class="js-add-to-cart" data-product-id=${
+                  product.id
+                }>Add</button>
               </div>
             </div>
           </div>
@@ -27,3 +36,26 @@ product.forEach((product) => {
       `;
 });
 document.querySelector(".product-grid").innerHTML = productHTML;
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({ productId: productId, quantity: 1 });
+    }
+
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    })
+    document.querySelector('.cart-quantity').innerHTML=cartQuantity;
+  });
+});
